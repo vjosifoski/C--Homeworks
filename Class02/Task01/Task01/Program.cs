914dc95d75b758167f2f91f738283171636a19be
vjosifoski <132778213+vjosifoski@users.noreply.github.com>
2024-03-01 19:48:42 +0100

    Add files via upload

---
+++ Class02/Task01/Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 15;
+            int num3 = 20;
+            int num4 = 30;
+
+            int avg = (num1 + num2 + num3 + num4) / 4;
+
+            Console.WriteLine($"The average of the numbers {num1}, {num2}, {num3}, and {num4} is: {avg}");
+
+
+            Console.WriteLine("Imput the First Number");
+            string inputFirstNumber = Console.ReadLine();
+            double convertedFirstNumber = Convert.ToDouble(inputFirstNumber);
+
+            Console.WriteLine("Imput the Second Number");
+            string inputSecondNumber = Console.ReadLine();
+            double convertedSecondNumber = Convert.ToDouble(inputSecondNumber);
+
+            Console.WriteLine("Imput the Third Number");
+            string inputThirdNumber = Console.ReadLine();
+            double convertedThirdNumber = Convert.ToDouble(inputThirdNumber);
+
+            Console.WriteLine("Imput the Forth Number");
+            string inputForthNumber = Console.ReadLine();
+            double convertedForthNumber = Convert.ToDouble(inputForthNumber);
+
+            double convertedResultAverageNumber = (convertedFirstNumber + convertedSecondNumber + convertedThirdNumber + convertedForthNumber) / 4;
+
+            Console.WriteLine($"The Converted Average Number out of the 4 is : {convertedResultAverageNumber}");
+        }
+    }
+}
---
+++ Class02/Task02/Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = 5;
+            double secondNumber = 8;
+
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+
+            Console.WriteLine($"This is the First Number: {firstNumber} and this is the Second Number: {secondNumber}");
+
+            Console.WriteLine("Imput the First Number");
+            string inputFirstNumber = Console.ReadLine();
+            double convertedFirstNumber = Convert.ToDouble(inputFirstNumber);
+
+            Console.WriteLine("Imput the Second Number");
+            string inputSecondNumber = Console.ReadLine();
+            double convertedSecondNumber = Convert.ToDouble(inputSecondNumber);
+
+            convertedFirstNumber = convertedFirstNumber + convertedSecondNumber;
+            convertedSecondNumber = convertedFirstNumber - convertedSecondNumber;
+            convertedFirstNumber = convertedFirstNumber - convertedSecondNumber;
+
+            Console.WriteLine($"This is the First Number: {convertedFirstNumber} and this is the Second Number: {convertedSecondNumber}");
+        }
+    }
+}
