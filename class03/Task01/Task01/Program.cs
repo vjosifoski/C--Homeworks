4868d5f5a292ff7b80768e6c25c2b6aed1e4e86c
vjosifoski <132778213+vjosifoski@users.noreply.github.com>
2024-03-11 16:56:34 +0100

    Add files via upload

---
+++ class03/Task01/Task01/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double parsedfirstNumber;
+            while (true)
+            {
+                Console.WriteLine("Please enter the First number");
+                string firstNumber = Console.ReadLine();
+                bool parsingResult1 = double.TryParse(firstNumber, out parsedfirstNumber);
+
+                if (!parsingResult1)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+                else
+                {
+
+                    break;
+                }
+
+            }
+
+
+            double parsedSecondNumber;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter the Second number");
+                string secondNumber = Console.ReadLine();
+                bool parsingResult2 = double.TryParse(secondNumber, out parsedSecondNumber);
+
+                if (!parsingResult2)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+                else
+                {
+
+                    break;
+                }
+
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter one of these +, -, *, /");
+                string sign = Console.ReadLine();
+
+                if (sign == "+" || sign == "-" || sign == "*" || sign == "/")
+                {
+                    if (sign == "+")
+                    {
+                        double sum = parsedfirstNumber + parsedSecondNumber;
+                        Console.WriteLine(sum);
+                    }
+                    else if (sign == "-")
+                    {
+                        double minus = parsedfirstNumber - parsedSecondNumber;
+                        Console.WriteLine(minus);
+                    }
+                    else if (sign == "*")
+                    {
+                        double multiply = parsedfirstNumber * parsedSecondNumber;
+                        Console.WriteLine(multiply);
+                    }
+                    else
+                    {
+                        double divide = parsedfirstNumber / parsedSecondNumber;
+                        Console.WriteLine(divide);
+                    }
+
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid symbol - Please enter one of these +, -, *, /");
+                }
+            }
+        }
+    }
+}
---
+++ class03/Task02/Таск02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Таск02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Please enter another number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            for (int i = secondNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
